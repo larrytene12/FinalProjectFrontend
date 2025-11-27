23d86255012c1d7bb97af4ce44a4edf670020d22
Larry Ray Tene <s22310220@student.unklab.ac.id>
2025-11-27 11:40:53 +0800

    Revert "Merge pull request #6 from larrytene12/larry/updatelagi"
    This reverts commit 24b656728a04309b734d751d242339308207aac7, reversing
    changes made to 0e3939384e2cd05f1b87d37d0d6d0c053d0d8b9b.

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const APP_CONFIG = {
-  NAME: "UNIVERSITAS KLABAT", 
-  SHORT_NAME: "Pendaftaran UNKLAB", 
-  LOGO_URL: "/unklab.png",
-  BG_IMAGE_URL: ""
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        primary: '#2563eb', 
-        secondary: '#f59e0b', 
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
+
